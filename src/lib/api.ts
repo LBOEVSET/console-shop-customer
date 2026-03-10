79847820c7ce39a2f695ebc28fe61b57ef00549f
Sorawich <sorawich.t@entronica.in.th>
2026-03-10 11:01:24 +0700

    update_api_module

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,13 @@
 import axios from "axios"
 import { initGuest } from "@/lib/guest"
 
+const baseURL =
+  typeof window === "undefined"
+    ? process.env.NEXT_API_URL
+    : process.env.NEXT_PUBLIC_API_URL;
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: baseURL,
   withCredentials: true,
 })
 
