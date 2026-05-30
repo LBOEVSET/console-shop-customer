808c767288a7fcfa9a4d9d747d00ec5ca98f8733
Sorawich <sorawich.t@entronica.in.th>
2026-05-30 16:46:45 +0700

    fix_order_data_type

--- src/types/order.ts
+++ src/types/order.ts
@@ -7,16 +7,20 @@ export interface Order {
   id: string
   status: string
   total: number
+  subtotal: number
+  discount: number
   createdAt: string
-  item:{
-    product: {
+  paymentMethod: string
+  payment?: { status: string }
+  item: {
+    title: string
+    quantity: number
+    price: number
+    productId?: string | null
+    eventId?: string | null
+    product?: {
       title: string
-      media: [
-        {
-          type: string
-          url: string
-        }
-      ]
-    }
+      media: { type: string; url: string }[]
+    } | null
   }
 }
